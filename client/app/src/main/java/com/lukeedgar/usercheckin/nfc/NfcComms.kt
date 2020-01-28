10711e67ceb063b7737fde433e9fc596c99dec60
Luke Edgar <lukeedgar@outlook.com>
2020-01-28 15:03:35 +0000

    Add basic NFC application

---
+++ client/app/src/main/java/com/lukeedgar/usercheckin/nfc/NfcComms.kt
@@ -0,0 +1,39 @@
+package com.lukeedgar.usercheckin.nfc
+
+import android.nfc.Tag
+import android.nfc.tech.MifareUltralight
+import java.nio.charset.Charset
+import android.nfc.NdefRecord
+
+
+class NfcComms {
+
+    fun getNdefRecord() : NdefRecord {
+        return NdefRecord.createMime(
+            "application/com.lukeedgar.usercheckin",
+            "Beam me up, Android".toByteArray(Charset.forName("US-ASCII"))
+        )
+    }
+
+    fun writeTag(tag: Tag, tagText: String) {
+        MifareUltralight.get(tag)?.use { ultralight ->
+            ultralight.connect()
+            Charset.forName("US-ASCII").also { usAscii ->
+                ultralight.writePage(4, "abcd".toByteArray(usAscii))
+                ultralight.writePage(5, "efgh".toByteArray(usAscii))
+                ultralight.writePage(6, "ijkl".toByteArray(usAscii))
+                ultralight.writePage(7, "mnop".toByteArray(usAscii))
+            }
+        }
+    }
+
+        fun readTag(tag: MifareUltralight): String? {
+            return tag.use { mifare ->
+                mifare.connect()
+                val payload = mifare.readPages(4)
+                String(payload, Charset.forName("US-ASCII"))
+            }
+        }
+
+
+}
---
+++ client/app/src/test/java/com/lukeedgar/usercheckin/nfc/NfcCommsTest.kt
@@ -0,0 +1,12 @@
+package com.lukeedgar.usercheckin.nfc
+
+import org.junit.Test
+
+class NfcCommsTest {
+
+    @Test
+    fun `should write account to tag`() {
+
+    }
+
+}
